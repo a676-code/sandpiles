848ef383a410e6e77771a51cdcc764151a7e02d3
thisusernameisaparadox <109701008+thisusernameisaparadox@users.noreply.github.com>
2022-12-11 15:06:50 -0500

    added proper color printing for some sandpiles

--- Sandpile.py
+++ Sandpile.py
@@ -1,3 +1,6 @@
+import numpy as np
+import colorama
+
 # Sandpile unequal maxes exception
 class UnequalMaxesException(Exception):
     "Raised when two combined sandpiles have different maxes"
@@ -26,6 +29,25 @@ class Sandpile:
 
     def getValues(self):
         return self.values
+
+    def __add__(self, o):
+        try:
+            if self.Max != o.Max:
+                raise UnequalMaxesException
+            elif self.Max != o.Max:
+                raise UnequalSizesException
+            else:
+                array = []
+                for i in range(self.size):
+                    row = []
+                    array.append(row)
+                    for j in range(self.size):
+                        array[i].append(self.values[i][j] + o.values[i][j])
+                return Sandpile(self.Max, self.size, array)
+        except UnequalMaxesException:
+            print("Exception occurred: Maxes not equal. Cannot add sandpiles")
+        except UnequalSizesException:
+            print("Exception occurred: Sizes not equal. Cannot add sandpiles")
     
     def checkValues(self):
         array = []
@@ -34,6 +56,62 @@ class Sandpile:
                 if (self.values[i][j] > self.Max):
                     array.append((i, j))
         return array
+
+    def color_int(self, x):
+        if x == 0:
+            c = colorama.Fore.BLACK
+        elif x == 1:
+            c = colorama.Fore.YELLOW
+        elif x == 2:
+            c = colorama.Fore.BLUE
+        elif x == 3:
+            c = colorama.Fore.RED
+        else:
+            c = colorama.Fore.GREEN
+        return f'{c}{x}'
+
+    def __eq__(self, o):
+        try:
+            if self.Max != o.Max:
+                raise UnequalMaxesException
+            elif self.size != o.size:
+                raise UnequalSizesException
+            else:
+                for i in range(self.size):
+                    for j in range(self.size):
+                        if self.values[i][j] != o.values[i][j]:
+                            return False
+                return True
+        except UnequalMaxesException:
+            print("Exception occurred: Maxes not equal. Cannot compare sandpiles")
+        except UnequalSizesException:
+            print("Exception occurred: Sizes not equal. Cannot compare sandpiles")
+
+    def print(self):
+        for i in range(int(self.size)):
+            print("\n", end='')
+            for j in range(int(self.size)):
+                print(self.color_int(self.values[i][j]), end = ' ')
+        print("\n", end='')
+
+    def readFile(self, fileName = None):
+        if fileName is None:
+            fileName = input("Enter a file name: ")
+        file = open(fileName, "r")
+        data = file.readlines()
+        for i in range(len(data)):
+            data[i] = data[i].strip("\n")
+        sandpile = []
+        sandpile.append(data[0])
+        sandpile.append(data[1])
+        values = []
+        for i in range(2, len(data)):
+            values.append(data[i].split(","))
+        sandpile.append(values)
+        self.Max = sandpile[0]
+        self.size = sandpile[1]
+        self.values = sandpile[2]
+        return self
 
     def topple(self):
         while self.checkValues():
@@ -76,66 +154,4 @@ class Sandpile:
                     else: # bottom right
                         self.values[p[0] - 1][p[1]] += 1
                         self.values[p[0]][p[1] - 1] += 1
-        return self
-
-    def print(self):
-        for i in range(int(self.size)):
-            print("\n", end='')
-            for j in range(int(self.size)):
-                print(self.values[i][j], end=' ')
-        print("\n", end='')
-
-    def __add__(self, o):
-        try:
-            if self.Max != o.Max:
-                raise UnequalMaxesException
-            elif self.Max != o.Max:
-                raise UnequalSizesException
-            else:
-                array = []
-                for i in range(self.size):
-                    row = []
-                    array.append(row)
-                    for j in range(self.size):
-                        array[i].append(self.values[i][j] + o.values[i][j])
-                return Sandpile(self.Max, self.size, array)
-        except UnequalMaxesException:
-            print("Exception occurred: Maxes not equal. Cannot add sandpiles")
-        except UnequalSizesException:
-            print("Exception occurred: Sizes not equal. Cannot add sandpiles")
-
-    def __eq__(self, o):
-        try:
-            if self.Max != o.Max:
-                raise UnequalMaxesException
-            elif self.size != o.size:
-                raise UnequalSizesException
-            else:
-                for i in range(self.size):
-                    for j in range(self.size):
-                        if self.values[i][j] != o.values[i][j]:
-                            return False
-                return True
-        except UnequalMaxesException:
-            print("Exception occurred: Maxes not equal. Cannot compare sandpiles")
-        except UnequalSizesException:
-            print("Exception occurred: Sizes not equal. Cannot compare sandpiles")
-
-    def readFile(self, fileName = None):
-        if fileName is None:
-            fileName = input("Enter a file name: ")
-        file = open(fileName, "r")
-        data = file.readlines()
-        for i in range(len(data)):
-            data[i] = data[i].strip("\n")
-        sandpile = []
-        sandpile.append(data[0])
-        sandpile.append(data[1])
-        values = []
-        for i in range(2, len(data)):
-            values.append(data[i].split(","))
-        sandpile.append(values)
-        self.Max = sandpile[0]
-        self.size = sandpile[1]
-        self.values = sandpile[2]
         return self
--- main.py
+++ main.py
@@ -1,8 +1,11 @@
 from Sandpile import Sandpile
+import numpy as np
+
+# np.set_printoptions(formatter={'int': color_int})
 
 twozero = Sandpile(3, 2, [[2, 2], [2, 2]])
 
-threes = Sandpile(3, 3, [[3, 3, 3], [3, 3, 3], [3, 3, 3]])
+threes_3 = Sandpile(3, 3, [[3, 3, 3], [3, 3, 3], [3, 3, 3]])
 threezero = Sandpile(3, 3, [[2, 1, 2], [1, 0, 1], [2, 1, 2]])
 
 a = Sandpile(3, 3, [[0, 0, 0], [0, 0, 0], [0, 0, 1]])
@@ -10,7 +13,7 @@ b = Sandpile(3, 3, [[1, 0, 0], [0, 0, 0], [0, 0, 0]])
 
 (a + b).topple().print()
 
-(threes + threezero).topple().print()
+(threes_3 + threezero).topple().print()
 
 sp = Sandpile(3, 4, [[2, 1, 2, 2], [1, 2, 1, 3], [3, 1, 3, 3], [3, 3, 3, 2]])
 fourzero = Sandpile(3, 4, [[2, 3, 3, 2], [3, 2, 2, 3], [3, 2, 2, 3], [2, 3, 3, 2]])
@@ -21,16 +24,20 @@ threeinverse = Sandpile(3, 3, [[3, 3, 3], [3, 1, 3], [3, 3, 3]])
 
 threezero.print()
 
-(threes + threeinverse).topple().print()
+(threes_3 + threeinverse).topple().print()
 
 
-if (threezero == (threes + threeinverse).topple()):
+if (threezero == (threes_3 + threeinverse).topple()):
     print("EQUAL!")
 else:
     print("NOT EQUAL!")
 
-blank = Sandpile(3, 3, [[], [], []])
-blank.readFile("threes_3.csv")
-
 blank2 = Sandpile(3, 5, [[], [], [], [], []])
-blank2.readFile("fivezero.csv")
+
+# prints green? 
+blank2.readFile("fivezero.csv").print()
+
+threezero.print()
+
+# prints green?
+blank2.print()
