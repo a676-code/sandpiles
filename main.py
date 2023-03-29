09452c1607f88c6084828f9ebcafcedcf3eecc27
a676-code <109701008+thisusernameisaparadox@users.noreply.github.com>
2023-03-29 13:18:56 -0400

    update main.py

--- main.py
+++ main.py
@@ -6,24 +6,24 @@ threes_3 = Sandpile(3, 3, 3, [[3, 3, 3], [3, 3, 3], [3, 3, 3]])
 threezero = Sandpile(3, 3, 3, [[2, 1, 2], [1, 0, 1], [2, 1, 2]])
 
 # 3 + 0 = 3
-(threes_3 + threezero).topple().print()
+(threes_3 + threezero).topple().print_sandpile()
 
 sp = Sandpile(3, 4, 4, [[2, 1, 2, 2], [1, 2, 1, 3], [3, 1, 3, 3], [3, 3, 3, 2]])
 fourzero = Sandpile(3, 4, 4, [[2, 3, 3, 2], [3, 2, 2, 3], [3, 2, 2, 3], [2, 3, 3, 2]])
 
 # sp + 0 = sp
-(sp + fourzero).topple().print()
+(sp + fourzero).topple().print_sandpile()
 
 threeinverse = Sandpile(3, 3, 3, [[3, 3, 3], [3, 1, 3], [3, 3, 3]])
 
 # 3 + (-3) = 0
-(threes_3 + threeinverse).topple().print()
+(threes_3 + threeinverse).topple().print_sandpile()
 
 blank = Sandpile(3, 5, 5, [[], [], [], [], []])
 fivezero = blank.readFile("fivezero.csv")
 
-fivezero.print()
+fivezero.print_sandpile()
 
 rectangle = Sandpile(3, 3, 5, [[], [], [], [], [] , [], [], [], []])
 
-rectangle.readFile("rectangle.csv").topple().print()
+rectangle.readFile("rectangle.csv").topple().print_sandpile()
